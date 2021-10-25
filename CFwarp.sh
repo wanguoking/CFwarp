70b73f81f38ccab8e631394cc3685df3fa98970a
kkkyg <90416692+kkkyg@users.noreply.github.com>
2021-10-25 15:25:31 +0800

    Update CFwarp.sh

--- CFwarp.sh
+++ CFwarp.sh
@@ -61,27 +61,27 @@ vi=`systemd-detect-virt >/dev/null 2>&1`
 
 if ! type curl >/dev/null 2>&1; then 
 if [ $release = "Centos" ]; then
-yellow "curl 未安装，安装中 "
+yellow " curl 未安装，安装中 "
 yum -y update && yum install curl -y
 else
 apt update -y && apt install curl -y
 fi	   
 else
-green "curl 已安装，继续 "
+green " curl 已安装，继续 "
 fi
 
 if ! type wget >/dev/null 2>&1; then 
 if [ $release = "Centos" ]; then
-yellow "curl wget，安装中 "
+yellow " wget 未安装，安装中 "
 yum -y update && yum install wget -y
 else
 apt update -y && apt install wget -y
 fi	   
 else
-green "wget 已安装，继续 "
+green " wget 已安装，继续 "
 fi 
 sleep 1s
-yellow "等待2秒……检测vps中……"
+yellow " 等待2秒……检测vps中……"
 AE="阿联酋";AU="澳大利亚";BR="巴西";CA="加拿大";CH="瑞士";CL="智利";CN="中国";DE="德国";ES="西班牙";FI="芬兰";FR="法国";HK="香港";ID="印尼";IE="爱尔兰";IL="以色列";IN="印度";IT="意大利";JP="日本";KR="韩国";MY="马来西亚";NL="荷兰";NZ="新西兰";PH="菲律宾";RU="俄罗斯";SA="沙特";SE="瑞典";SG="新加坡";TW="台湾";UK="英国";US="美国";VN="越南";ZA="南非"
 
 v44=`wget -T1 -t1 -qO- -4 ip.gs`
